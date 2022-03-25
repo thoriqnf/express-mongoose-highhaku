522f2c01217ea54e0ae059e9c27d6563b48c3f5c
thoriqnf <thoriqnfaizal@gmail.com>
2022-03-25 09:41:48 +0700

    changing path using env

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-const uri =
-  "mongodb+srv://skilvul:skilvul123@cluster0.wfybr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = process.env.MONGODBURI;
 
 const db = mongoose.connect(uri);
 
